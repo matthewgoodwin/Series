278998ecf70632fba13e389a5a4ac18c96c3fa47
matthew goodwin <matthewjgoodwin@gmail.com>
2017-01-11 02:58:29 +0900

    dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :email, :username, :firstname, :lastname, presence: true
   validates :email, uniqueness: true
 
-  has_many :user_shows
+  has_many :user_shows, dependent: :destroy
   has_many :shows, through: :user_shows
 
 end
