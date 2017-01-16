cb02bb006d1b36b5c106e3b7281d107fd8c5e696
matthew goodwin <matthewjgoodwin@gmail.com>
2017-01-17 03:26:16 +0900

    adding nested reviews, still needs model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "pages#home"
-  resources :user_shows, only:[:index,:show,:create,:destroy]
+  resources :user_shows, only:[:index,:show,:create,:destroy] do
+    resources :reviews, only:[:new, :create, :show, :destroy]
+  end
   resources :shows, only: [:show]
   get "contact", to: "pages#contact"
   get "about", to: "pages#about"
