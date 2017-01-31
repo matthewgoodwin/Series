5e84f056d28b16af8e352918d905cef6ed4435bf
matthew goodwin <matthewjgoodwin@gmail.com>
2017-01-31 20:51:11 +0900

    review model validations, html class added to  _review_form partial, review _show partial altered from h4 to paragraph to allow breaks, clear all form data when review is saved

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user_show
 
   validates :phrase, :rating, :standout, presence: true
+  validates :phrase, length: { maximum: 40, too_long: "max of %(count)characters per review!"}
   validates :rating, inclusion: {in: (1..5), message: "rating must be between 1 and 5 stars!"}
   validates :standout, inclusion: {in: ["Season 1", "Season 2", "Season 3","Season 4","Season 5","Season 6","Season 7","Season 8","Season 9","Season 10"], message: "must be a valid season!"}
   validates :standout, uniqueness: {scope: :user_show, message: "only one review per season!"}
